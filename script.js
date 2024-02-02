b262f74f404b4c23b6f0bd7c5c7181b555097654
igeon22 <griffley2080@gmail.com>
2024-02-02 08:57:32 -0500

    Finished the DOM game version

--- script.js
+++ script.js
@@ -1,148 +1,119 @@
-let gameBoard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-// let gameBoard = ['O',' ',' ','O',' ',' ','O',' ',' '];
+let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let gameBoardDom = document.querySelectorAll(".case");
+let resetBtn = document.querySelector(".reset");
 
+const Player = function (name, marker, actualTurn) {
+  let score = 0;
+  function setScore() {
+    score += 1;
+  }
 
-const Player = function(name,marker,actualTurn){
+  function getScore() {
+    return score;
+  }
+  return { setScore, getScore };
+};
 
-    let score = 0;
-    function sayHello(){
-        console.log("Hi, I'm here! "+name);
-        console.log(gameBoard);
-    };
+const getMarker = function (t) {
+  if (t % 2 === 0) {
+    return "X";
+  } else {
+    return "O";
+  }
+};
 
-    function play(){
-        let a = true;
-        
-        // let condition = ()
-        do {
-            choice = prompt("Enter the case: ");
-            Number(choice);
-            choice = choice - 1;
-            console.log(choice);
-            if(gameBoard[choice] != ' ' || (choice<0 ||  choice>gameBoard.length)){
-                console.log("Error");
-            }else{
-                gameBoard[choice] = marker;
-                actualTurn += 1;
-                console.log(actualTurn,"suhushu");
-                a = false;
-            }
-            
-        } while (a === true);
-        console.log(gameBoard);
+const newGame = function () {
+  for (let i = 0; i < gameBoardDom.length; i++) {
+    gameBoard[i] = gameBoardDom[i].innerHTML;
+  }
 
+  let player1 = Player("Player 1", "X");
+  let player2 = Player("Player 2", "O");
+  let finished = false;
+
+  const resetGame = function () {
+    gameBoardDom.forEach((board) => {
+      board.textContent = "";
+    });
+
+    for (let i = 0; i < gameBoardDom.length; i++) {
+      gameBoard[i] = gameBoardDom[i].innerHTML;
     }
 
-    function setScore(){
-        score += 1;
+    finished = false;
+  };
+
+  gameBoardDom.forEach((p) => {
+    p.addEventListener("click", () => {
+      if (p.textContent === "" && finished === false) {
+        let t = checkTurn();
+        p.textContent += getMarker(t);
+        for (let i = 0; i < gameBoardDom.length; i++) {
+          gameBoard[i] = gameBoardDom[i].innerHTML;
+        }
+        let winner = gameStateCheck();
+        a = checkTurn();
+        if (winner != "none" || a >= 9) {
+          wins(winner, player1, player2, a);
+          finished = true;
+          document.querySelector(
+            ".win"
+          ).textContent = `X: ${player1.getScore()} || O: ${player2.getScore()}`;
+        }
+      }
+    });
+  });
+
+  resetBtn.addEventListener("click", () => {
+    resetGame();
+  });
+
+  const gameStateCheck = function () {
+    const winningComb = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let winner = "none";
+
+    winningComb.forEach((comb) => {
+      if (
+        gameBoard[comb[0]] === gameBoard[comb[1]] &&
+        gameBoard[comb[1]] === gameBoard[comb[2]]
+      ) {
+        if (gameBoard[comb[0]] != "") {
+          winner = gameBoard[comb[0]];
+        }
+      }
+    });
+
+    return winner;
+  };
+
+  const wins = function (winner, player1, player2, turn) {
+    if (winner === "X") {
+      player1.setScore();
+    } else if (winner === "O") {
+      player2.setScore();
+    } else if (turn >= 9) {
+      alert("It's a tie!");
     }
+  };
 
-    function getScore(){
-        return score;
-    }
-
-    function getTurn(){
-        return actualTurn;
-    }
-
-    
-
-
-
-    return {sayHello,play,actualTurn,getTurn,setScore,getScore};
-}
-
-const newGame =  function(){
-
-    let score1 = 0;
-    let score2 = 0;
-
-
-    const displayBoard = function(){
-        console.log(`${gameBoard[0]}|${gameBoard[1]}|${gameBoard[2]}`);
-
-        console.log(`${gameBoard[3]}|${gameBoard[4]}|${gameBoard[5]}`);
-
-        console.log(`${gameBoard[6]}|${gameBoard[7]}|${gameBoard[8]}`);
-    }
-
+  const checkTurn = function () {
     let turn = 0;
-
-    const gameStateCheck = function(){
-        const winningComb = [[0,1,2],[3,4,5],[6,7,8], [0,3,6],[1,4,7],[2,5,8], [0,4,8],[2,4,6]];
-        let winner = "none";
-
-        winningComb.forEach((comb) => {
-            // console.log(comb[0]);
-            // console.log(gameBoard[index]);
-            if((gameBoard[comb[0]] === gameBoard[comb[1]]) && (gameBoard[comb[1]] === gameBoard[comb[2]])){
-                if(gameBoard[comb[0]] != ' '){
-                    console.log(gameBoard[comb[0]] + "WIN!!!");
-                    winner =  gameBoard[comb[0]];
-                }
-            }
-
-            
-        });
-        
-        return winner;
-    }
-
-    const wins = function(winner,player1,player2){
-        if(winner === 'X'){
-            player1.setScore();
-            alert(`Player 1: ${player1.getScore()}`)
-        }else if(winner === 'O'){
-            player2.setScore();
-            alert(`Player 2: ${player2.getScore()}`)
-        }else{
-            alert("It's a tie!")
-        }
-    }
-
-    const checkTurn = function(){
-        let turn = 0;
-        gameBoard.forEach((place)=>{
-            if(place != ' '){
-                turn += 1;
-            }
-        });
-
-        return turn;
-    }
-
-    const startGame =  function(){
-        let winner = "none";
-        let player1 = Player("Joe Dash","X",turn);
-        let player2 = Player("Jiguli","O",turn);
-        while(turn<=8 && winner === "none"){
-            console.log("Winner "+winner);
-
-            player1.play()
-            // console.log(player1.getTurn());
-            turn = checkTurn();
-            // console.log("Turn"+player1.actualTurn);
-            console.log("Actual Turn: ",turn);
-            displayBoard();
-            winner = gameStateCheck();
-    
-            if(turn>=8 || winner != "none"){
-                break;
-            }
-
-        
-            
-            player2.play()
-            turn = checkTurn();
-            displayBoard();
-            winner = gameStateCheck();
-            console.log("Winner "+winner);
-
-        }
-
-        wins(winner,player1,player2);
-
-    }
-}
+    gameBoard.forEach((place) => {
+      if (place != "") {
+        turn += 1;
+      }
+    });
+    return turn;
+  };
+};
 
 newGame();
